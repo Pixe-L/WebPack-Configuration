95c597775d1ea8ebd20b6116bc83af0fa2341ac4
Arturo Castañeda <tutospixel@hotmail.com>
2022-10-19 15:21:51 -0500

    First commit, webpack configuration

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {saludar} from './js/app';
+import './styles.css';
+
+const nombre = 'Arturo';
+
+// saludar(nombre);
---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+import '../css/components.css';
+import webpacklogo from '../assets/img/webpack-logo.png';
+
+export const saludar = (nombre) => {
+  console.log('Creando un h1');
+
+  const h1 = document.createElement('h1');
+  h1.innerText = `Hola, ${nombre}!!`;
+
+  document.body.append(h1);
+};
+
+saludar('Arturo');
+
+// img
+
+// console.log(webpacklogo);
+// const img = document.createElement('img');
+// img.src = webpacklogo;
+// document.body.append(img);
